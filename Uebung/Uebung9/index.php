20303c8fe1bef704b3097df59153a4af0e954ea5
nyx <nyxnyschd@posteo.de>
2020-05-19 17:25:23 +0200

    Klausur

--- Uebung/Uebung9/index.php
+++ Uebung/Uebung9/index.php
@@ -1,43 +1,78 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Uebung 9</title>
 
-    <style>
-        header{width:100%; background-color: #888888; text-align:center; }
-        h1{color:white; padding: 10px}
-
-
-    </style>
     <?php
     require_once "./player.php";
     ?>
+    <style>
+        header{width:100%; background-color: #888888; text-align:center; height: 80px }
+        h1{color:white; padding: 10px}
+        footer{position: absolute; bottom:0px;background-color:#888888; width:100%; text-align:center }
+        footer>a, footer>a:visited, footer>a:hover, footer>a:link{ text-decoration:none; color:white; text:bold}
+        #butt{background-color:#0082D1; color: white; border-radius: 5px; padding: 5px; margin: 5px}
+        #in {width:80%; padding: 5px; margin: 5px}
+
+        @media screen and (max-width: 768px){
+            #mitte{display: none};
+        }
+        #main {width:100%}
+
+    </style>
+    <title>Uebung 9</title>
+
 </head>
-<body>
+<div class="container-fluid">
+
     <header>
-        <h1>Ein Spiel</h1>
+        <h1 id="ueberschrift">Ein Spiel</h1>
 
     </header>
+    <body>
 
+    <div id="main" class= "row" >
+        <div id="links" class="col-sm-12 col-md-3">
+            <!-- wenn kleiner view: eine ganze Spalte allein -->
+            <h3> Auswahl Spielerin</h3>
+            <ul class="list-group">
+                <?php
+                for($i=0; $i<count($player);$i++){
+                    echo '<li> <a href="index.php? id='.$i.'"> '.$player[$i]["name"].'('.$player[$i]["moves"].')</a> </li>';
+                }
+                if (isset($_GET))
+                {
 
-<div class="container-fluid">
-    <section class=“row“>
-        <div class=“col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 >
-            // wenn kleiner view: nur eine Spalte,bei sm 2 spalten à 6...
-            <h3> Spalte1></h3>
+                }
+                ?>
+
         </div>
+
+        <div id="mitte" class="col-md-2"   >
+
+            <h3> oder</h3>
+        </div>
+
+        <div id="rechts" class="col-sm-12 col-md-7"  >
+            <h3> Neue Spielerin anlegen</h3>
+            <form action="index.php" method="post">
+               <!-- <label for #newPlayer></label> -->
+                <input type="text" placeholder="Name" name="spielerin" id="in">
+                <button id="butt" type="submit">Neue Spielerin anlegen</button>
+            </form>
+        </div>
+    </div>
 </div>
 
 <script>
+    function createFocus(event){
+        let plays = event.currentTarget[name];
+        document.getElementById("ueberschrift").innerHTML= plays+" spielt";
+    }
 
 </script>
 
 </body>
 
 <footer>
-    <a href=" ">Nyco C.Bischoff</a>
+    <a href="http://webtech.f4.htw-berlin.de/~s0569853/WebTech19/Uebung/">Nyco C.Bischoff</a>
 </footer>
 </html>
